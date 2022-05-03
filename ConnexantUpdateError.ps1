595fd95f6cbc36884e65026d307b0faee3b0f28f
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-05-03 19:56:21 +0200

    Update ConnexantUpdateError.ps1

--- ConnexantUpdateError.ps1
+++ ConnexantUpdateError.ps1
@@ -5,6 +5,7 @@ Start-Process -Wait -FilePath "C:\Program Files\CONEXANT\CNXT_AUDIO_HDA\UIU64a.e
 Stop-Service -Name "wuauserv" -Force
 Remove-Item -Path "C:\Windows\SoftwareDistribution\Download" -Recurse
 #Remove-Item "C:\Windows\System32\DriverStore\FileRepository\cisstrt.inf_amd64_c4cece4cb44ec2e5\cisstrt.inf" -Force
+Remove-WindowsDriver -Path "C:\Windows\System32\DriverStore\FileRepository" -Driver "cisstrt.inf"
 
 # start windows update
 control update
@@ -13,11 +14,13 @@ UsoClient StartInteractiveScan
 
 # tworzenie jednorazowego skryptu "EnableSound.bat"
 New-Item -Path "C:\Temp\EnableSound.txt" -ItemType File
-Set-Content -Path "C:\Temp\EnableSound.txt" -Value 'powershell.exe  -NoProfile -ExecutionPolicy Bypass -Command "& {Get-PnpDevice -FriendlyName "Conexant ISST Audio" | Enable-PnpDevice -confirm:$false}"'
+Add-Content -Path "C:\Temp\EnableSound.txt" -Value 'powershell.exe  -NoProfile -ExecutionPolicy Bypass -Command "& {Get-PnpDevice -Class Media | Enable-PnpDevice -confirm:$false}"'
+Add-Content -Path "C:\Temp\EnableSound.txt" -Value 'powershell.exe  -NoProfile -ExecutionPolicy Bypass -Command "& {Get-WindowsDriver -Online -All}"'
 Rename-Item -Path "C:\Temp\EnableSound.txt" -NewName "C:\Temp\EnableSound.bat"
 
 # dodaje jednorazowy klucz rejestru, ktory uruchamia EnableSound.bat przy nastepnym restarcie
-.\reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v !EnableSound /t REG_SZ /d "\"C:\Temp\EnableSound.bat\""
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v !EnableSound /t REG_SZ /d "C:\Temp\EnableSound.bat"
 
-
+Write-Host "PRESS ANY KEY TO RESTART?"
 pause
+Restart-Computer
