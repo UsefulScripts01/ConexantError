200cf33329dbe5f183db5c0afa959c3b6f1f4af3
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-05-03 21:42:58 +0200

    Update EnableSound.bat

--- EnableSound.bat
+++ EnableSound.bat
@@ -1,3 +1,4 @@
 powershell.exe -executionpolicy bypass Get-CimInstance -Class Win32_PNPEntity -Property Manufacturer -Filter 'Manufacturer LIKE "%conexant%"' | Disable-PnpDevice -confirm:$false
 control update
 UsoClient StartInteractiveScan
+pause
