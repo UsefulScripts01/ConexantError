073c74f486190ab770e5987c6b2606d9a5f46bc7
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-05-03 19:37:15 +0200

    Update ConnexantUpdateError.ps1

--- ConnexantUpdateError.ps1
+++ ConnexantUpdateError.ps1
@@ -1,6 +1,5 @@
 Get-PnpDevice -FriendlyName "Conexant ISST Audio" | Disable-PnpDevice -confirm:$false
 
-
 Start-Process -Wait -FilePath "C:\Program Files\CONEXANT\CNXT_AUDIO_HDA\UIU64a.exe" -ArgumentList "-U -R -G -S -Icisstrt.inf -OI=IntcAudioBus.inf"
 
 Stop-Service -Name "wuauserv" -Force
@@ -14,10 +13,11 @@ UsoClient StartInteractiveScan
 
 # tworzenie jednorazowego skryptu "EnableSound.bat"
 New-Item -Path "C:\Temp\EnableSound.txt" -ItemType File
-Set-Content -Path "C:\Temp\EnableSound.txt" -Value 'powershell.exe  -NoProfile -ExecutionPolicy Bypass -Command "& {Get-PnpDevice -FriendlyName "*Conexant*" -Class MEDIA | Enable-PnpDevice -confirm:$false}"'
+Set-Content -Path "C:\Temp\EnableSound.txt" -Value 'powershell.exe  -NoProfile -ExecutionPolicy Bypass -Command "& {Get-PnpDevice -FriendlyName "Conexant ISST Audio" | Enable-PnpDevice -confirm:$false}"'
 Rename-Item -Path "C:\Temp\EnableSound.txt" -NewName "C:\Temp\EnableSound.bat"
 
 # dodaje jednorazowy klucz rejestru, ktory uruchamia EnableSound.bat przy nastepnym restarcie
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v !EnableSound /t REG_SZ /d "\"C:\Temp\EnableSound.bat\""
+.\reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v !EnableSound /t REG_SZ /d "\"C:\Temp\EnableSound.bat\""
+
 
 pause
